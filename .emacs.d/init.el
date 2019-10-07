03e2f5d196d518a65b36de4c37856c70bee1f34a
simenojensen <simen@omholt-jensen.com>
2019-10-07 15:47:50 +0200

    add org-mode-test.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,6 +18,13 @@
   :ensure t
   :config (which-key-mode))
 
+;; Org-mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+
 
 ;; Automatically added stuff
 (custom-set-variables
@@ -25,7 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages (quote (org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
