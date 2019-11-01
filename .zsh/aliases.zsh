d72ff9320c33a7b383d4a55b5e0b5bafa5c238e7
simenojensen <simen@omholt-jensen.com>
2019-11-01 14:56:58 +0100

    add show/hide desktop

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -36,3 +36,8 @@ alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resource
 
 # stop ping after 10 sends
 alias ping='ping -c 10'
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
